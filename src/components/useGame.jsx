1699ca544caf2254a38791533f374d25ec410afe
Студенческая <@rbiu.local>
2026-04-08 11:02:47 +0500

    game logic

---
+++ src/components/useGame.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import images from "../data.json"
+
+export default function useGame(){
+    //чписок отгаданых карточек
+    const [finishedItems, setfinishedItems] = useState([]) //выбрать из списка
+    //счестчик шагов
+    const[stepsCount, setStepsCount] = useState(0);
+    //функция сравнения 2 карточек
+    const checkItems = (firstItem,secondItem) => {
+        //находим первую картинку
+        const firstImage = images.find (({id}) => id == firstItem)//куча скобок это деструктуризация
+        //находим первую картинку
+        const secondImage = images.find (({id}) => id == secondItem)
+        //условие
+        if(firstImage.url == secondImage.url){ //если картинки одинаковые
+            //добавляем отгаданные карточки в массив
+            setfinishedItems((items)=>[...items,firstItem,secondItem])
+        }
+        setStepsCount(stepsCount + 1) //увеличиваем счетчик шагов на 1
+    }
+    //функция сброса игры
+    const handleReset = () => {
+        setfinishedItems([]) //обнуляем массив с отгаданными карточками
+        setStepsCount(0) //обнуляем счетчик шагов
+    }
+    //проверка конца игры
+    const isWin = finishedItems.length == images.length
+
+    // то что мы будем возвращать
+    return{
+        finishedItems,
+        stepsCount,
+        isWin,
+        handleReset,
+        checkItems
+    }
+}
+
+
--- src/pages/PageGame/PageGame.jsx
+++ src/pages/PageGame/PageGame.jsx
@@ -2,17 +2,48 @@ import { Link } from "react-router-dom";
 import Button from "../../components/Button/Button";
 import Grid from "../../components/Grid/Grid";
 import Header from "../../components/Header/Header";
+import useGame from "../../components/useGame";
+
+import images from "../../data.json"
+import Modal from "../../components/Modal/Modal";
 
 export default function PageGame (){
-    return (
+  const { finishedItems,
+        stepsCount,
+        isWin,
+        handleReset,
+        checkItems} = useGame() //импортируем с деструктуризацией
+
+  const handlBtnReset = () => {
+    handleReset();
+    //перемешивать массив
+    images.sort(() => Math.random() -0.5)
+  }
+  
+    return   (
         <div className='container'>
           <Header/>
           <main>
-            <Button title='заказакть' color="red" rounded="false" />
-             <Button  title='купить' color="blue"/>
-              <Button  title='войти' />
-              <button>моя кнопка</button>
-            <Grid/>
+            <div className="steps">{stepsCount}</div>
+
+            <Grid 
+            images={images}
+            finishedItems ={ finishedItems}
+            checkItems = {checkItems}
+            />
+            {
+              isWin && (
+                <Modal>
+                  <h3 className="modal-caption">Победа!</h3>
+                  <p className=".modal-description"> Вы собрали все пары за {stepsCount} шагов</p>
+                  <button 
+                  className="button modal-button"
+                  onClick={handlBtnReset}
+                  type="Button">новая игра</button>
+                </Modal>
+              )
+            }
+
           </main>
           <footer>
             <p>&cope; Сухоплюева Екатерина, 2026 г.</p>
